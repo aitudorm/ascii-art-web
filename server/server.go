0c8b4ba9c314770fc060eb2cb99ff71d274f7152
aitudorm <77835933+aitudorm@users.noreply.github.com>
2022-08-30 14:18:54 +0600

    Update server.go

--- server/server.go
+++ server/server.go
@@ -4,10 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"flag"
 )
 
 func Server() {
 	mux := http.NewServeMux()
+	
+	port := flag.Int("port", -1, "specify a port to use http rather than AWS Lambda")
+    flag.Parse()
+    listener := gateway.ListenAndServe
+    portStr := "n/a"
+    if *port != -1 {
+        portStr = fmt.Sprintf(":%d", *port)
+        listener = http.ListenAndServe
+        http.Handle("/", http.FileServer(http.Dir("./ui")))
+    }
+
 
 	fileServer := http.FileServer(http.Dir("./ui"))
 	mux.Handle("/ui/", http.StripPrefix("/ui", fileServer))
